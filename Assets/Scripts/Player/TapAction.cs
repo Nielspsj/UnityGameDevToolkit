a317feffa0283e103b878797dc0ede8d979c5516
Nielspsj <nielspsj@hotmail.com>
2025-10-27 22:39:41 +0100

    Proper jumping added
    can now select between endless and single jumping in tap action script

--- Assets/Scripts/Player/TapAction.cs
+++ Assets/Scripts/Player/TapAction.cs
@@ -1,17 +1,32 @@
 using UnityEngine;
+using static SwerveController;
 
 public class TapAction : MonoBehaviour
 {
+    public enum JumpMode
+    {
+        Endless,   // Jump or flap wings forever
+        Single   // Once and then require ground check for more
+    }
+    [SerializeField, Header("Switch between jump modes")]
+    private JumpMode jumpMode = JumpMode.Endless; // Switch mode in Inspector
+
     [SerializeField, Header("Force applied upward when tapping")]
     private float jumpForce = 5f;
+    [SerializeField, Header("How far to check for ground")]
+    private float groundCheckDistance = 1.1f; // How far down to check for ground
+    [SerializeField, Header("Put the ground in the groundLayer")]
+    private LayerMask groundLayer;
 
     private Rigidbody rb;
+    private bool isGrounded = true;                // Store if player is touching the ground
+
 
     private void Awake()
     {
         if (InputManager.Instance != null)
         {
-            InputManager.Instance.OnTap.AddListener(Jump);
+            InputManager.Instance.OnTap.AddListener(HandleTap);
         }
         rb = GetComponent<Rigidbody>();
     }    
@@ -29,12 +44,45 @@ public class TapAction : MonoBehaviour
     {
         if (InputManager.Instance != null)
         {
-            InputManager.Instance.OnTap.RemoveListener(Jump);
+            InputManager.Instance.OnTap.RemoveListener(HandleTap);
         }
     }
 
-    private void Jump()
+    public void HandleTap()
     {
+        if (jumpMode == JumpMode.Endless)
+        {
+            JumpEndless();
+        }
+        else if (jumpMode == JumpMode.Single)
+        {
+            JumpSingle();
+        }
+    }
+
+    private void JumpEndless()
+    { 
         rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void JumpSingle()
+    {
+        // --- Ground Check ---
+        // Shoot a ray downward from the player's position
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, groundCheckDistance, groundLayer))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        // --- Jump Input ---
+        if (isGrounded == true)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
